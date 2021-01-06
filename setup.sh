49c076d310749627659e2c075c24969632a9aeb3
Arne Christoph Reimers <arne.reimers@physik.uzh.ch>
2021-01-06 10:18:48 +0100

    Added Submitter to setup script

--- setup.sh
+++ setup.sh
@@ -1,16 +1,17 @@
 # This script must be sourced from this exact folder before working in this folder.
 
 
-# Append path to our own libraries to LD_LIBRARY_PATH, such that our programs can use them!
+# Append path to our own libraries and paths to LD_LIBRARY_PATH and PATH and PYTHONPATH, such that our programs can use them!
 export MACROPATH=$(readlink -f macros)
 export MACROLIBPATH=${MACROPATH}/lib
 export MACROBINPATH=${MACROPATH}/bin
+export SUBMITTERPATH=${MACROPATH}/Submitter
 export PLOTTERPATH=${MACROPATH}/Plotter
 export PLOTTERBINPATH=${PLOTTERPATH}/bin
 export GENSIMPATH=$(readlink -f GENSIM)
 export NANOAODTOOLS_BASE=${GENSIMPATH}/NanoAODTools
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$MACROLIBPATH
-export PATH=${PATH}:${MACROBINPATH}:${PLOTTERBINPATH}
+export PATH=${PATH}:${MACROBINPATH}:${PLOTTERBINPATH}:${SUBMITTERPATH}
 export PYTHONPATH=$PYTHONPATH:$GENSIMPATH
 export PYTHONPATH=${PYTHONPATH}:${NANOAODTOOLS_BASE}/build/lib/python
