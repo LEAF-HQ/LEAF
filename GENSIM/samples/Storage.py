81fe6db619879d924599bc50440b46ee5ecf3877
Arne Christoph Reimers <arne.reimers@physik.uzh.ch>
2021-02-06 11:54:25 +0100

    Switched QCD samples to NanoAOD v8. Need to check for other samples in some time

--- GENSIM/samples/Storage.py
+++ GENSIM/samples/Storage.py
@@ -12,7 +12,7 @@ class Storage_DAS(Storage):
     def __init__(self, path):
         self.director = 'root://cms-xrd-global.cern.ch/'
         self.path = path
-        self.filelist = self.get_file_list()
+        # self.filelist = self.get_file_list()
 
     def get_file_list(self):
         #call 'dasgoclient' to get a list of rootfiles from the DAS string
@@ -29,7 +29,7 @@ class Storage_T2PSI(Storage):
     def __init__(self, path):
         self.director = 'root://storage01.lcg.cscs.ch/'
         self.path = '/pnfs/lcg.cscs.ch/cms/trivcat' + path
-        self.filelist = self.get_file_list()
+        # self.filelist = self.get_file_list()
 
     def get_file_list(self):
         command = 'LD_LIBRARY_PATH='' PYTHONPATH='' gfal-ls %s' % (self.director.replace('root://', 'gsiftp://')+self.path)
@@ -45,7 +45,7 @@ class Storage_T3PSI(Storage):
     def __init__(self, path):
         self.director = 'root://t3dcachedb03.psi.ch/'
         self.path = '/pnfs/psi.ch/cms/trivcat' + path
-        self.filelist = self.get_file_list()
+        # self.filelist = self.get_file_list()
 
     # if the path does not yet exist, create it
     def make_dirs(self):
