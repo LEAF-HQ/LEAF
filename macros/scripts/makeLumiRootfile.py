31df13306c580dcd97638b92db0e0e08c1fb3f75
Arne Christoph Reimers <arne.reimers@physik.uzh.ch>
2021-02-24 00:26:02 +0100

    Added scipt to transform golden/silver/... JSON files in .txt format into .root files that can be read by the main framework.

---
+++ macros/scripts/makeLumiRootfile.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+import os, sys, math, time, argparse
+import json
+from yaml import safe_load
+import ROOT
+from ROOT import TFile, TTree
+from array import array
+from utils import green as green
+
+
+# Usage: ./makeLumiRootfile.py -i <JSON_FILE_NAME.txt> (-o <OUTFILENAME.root>)
+# If no outfilename is give, it will be the same as the infilename, but with '.txt' replaced by '.root'.
+
+def main():
+    parser = argparse.ArgumentParser(description='Program that takes as an argument the json -->.txt<-- file and makes a root file out of it, which can be used with the main framework.')
+    parser.add_argument('--input', '-i' , required=True, type=str, help='Name of the json converted to a .txt file')
+    parser.add_argument('--output', '-o', type=str, help='Name of the target .root file')
+
+    args = parser.parse_args()
+    infilename  = os.path.abspath(args.input)
+    if not infilename.endswith('.txt'):
+        raise ValueError('Infilename does not end with .txt')
+    outfilename = os.path.abspath(args.output) if args.output is not None else '.root'.join(infilename.rsplit('.txt', 1))
+
+    with open(infilename, 'r') as f:
+        dict_in_json = safe_load(f)
+
+    outfile = TFile(outfilename, 'RECREATE')
+    outtree = TTree("LumiTree","LumiTree")
+    t_run  = array('i',[0])
+    t_lb   = array('i',[0])
+
+    outtree.Branch("run", t_run,  'run number/i')
+    outtree.Branch("lb",  t_lb,   'lumi block/i')
+    runs = dict_in_json.keys()
+    for run in dict_in_json:
+        parts = dict_in_json[run]
+        for lumiblocks in parts:
+            for lb in range(lumiblocks[0], lumiblocks[1]+1):
+                t_run[0] = int(run)
+                t_lb[0]  = int(lb)
+                outtree.Fill()
+                # print 'filled: (%i, %i)' % (int(run), int(lb))
+
+    outfile.Write()
+    outfile.Close()
+    print green('--> Successfully created lumifile \'%s\'' % (outfilename))
+
+
+
+if __name__ == '__main__':
+    main()
