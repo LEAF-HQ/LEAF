9e204d6cafddc448396b83eb18a4207c8c5bb81d
Arne Christoph Reimers <arne.reimers@physik.uzh.ch>
2021-03-10 11:52:52 +0100

    Added flag and invariant-mass selections

---
+++ macros/include/MinvSelections.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "include/Config.h"
+#include "include/Selection.h"
+#include "include/RecoEvent.h"
+#include "include/ObjectIdUtils.h"
+
+#include <vector>
+// #include <pair>
+
+class MemuSelection: public Selection<RecoEvent>{
+public:
+  explicit MemuSelection(const Config & cfg, int mmin_, int mmax_);
+  virtual bool passes(RecoEvent & event) override;
+
+private:
+  double mmin, mmax;
+
+};
+
+class MetauSelection: public Selection<RecoEvent>{
+public:
+  explicit MetauSelection(const Config & cfg, int mmin_, int mmax_);
+  virtual bool passes(RecoEvent & event) override;
+
+private:
+  double mmin, mmax;
+
+};
+
+class MmutauSelection: public Selection<RecoEvent>{
+public:
+  explicit MmutauSelection(const Config & cfg, int mmin_, int mmax_);
+  virtual bool passes(RecoEvent & event) override;
+
+private:
+  double mmin, mmax;
+
+};
