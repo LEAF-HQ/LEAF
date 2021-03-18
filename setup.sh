ec4343ebf37edd182ffcf79aa0b238021e976895
Arne Christoph Reimers <arne.reimers@physik.uzh.ch>
2021-03-18 18:54:11 +0100

    Renamed 'GENSIM' to 'Generator' and adjusted setup.sh. Will probably fail, need to propagate these changes to more scripts.

--- setup.sh
+++ setup.sh
@@ -2,16 +2,17 @@
 
 
 # Append path to our own libraries and paths to LD_LIBRARY_PATH and PATH and PYTHONPATH, such that our programs can use them!
+export LEAFPATH=$(readlink -f .)
 export MACROPATH=$(readlink -f macros)
 export MACROLIBPATH=${MACROPATH}/lib
 export MACROBINPATH=${MACROPATH}/bin
 export SUBMITTERPATH=$(readlink -f Submitter)
 export PLOTTERPATH=$(readlink -f Plotter)
 export PLOTTERBINPATH=${PLOTTERPATH}/bin
-export GENSIMPATH=$(readlink -f GENSIM)
-export NANOAODTOOLS_BASE=${GENSIMPATH}/NanoAODTools
+export GENERATORPATH=$(readlink -f Generator)
+export NANOAODTOOLS_BASE=${GENERATORPATH}/NanoAODTools
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$MACROLIBPATH
 export PATH=${PATH}:${MACROBINPATH}:${PLOTTERBINPATH}:${SUBMITTERPATH}
-export PYTHONPATH=$PYTHONPATH:$GENSIMPATH
+export PYTHONPATH=$PYTHONPATH:$GENERATORPATH
 export PYTHONPATH=${PYTHONPATH}:${NANOAODTOOLS_BASE}/build/lib/python
