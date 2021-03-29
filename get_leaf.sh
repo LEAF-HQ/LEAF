0a23a85e3d34fc3682d504ebf4baf1b5d91c7961
Arne Christoph Reimers <arne.reimers@physik.uzh.ch>
2021-03-29 18:15:51 +0200

    small changes to setup

--- get_leaf.sh
+++ get_leaf.sh
@@ -5,6 +5,14 @@ export SCRAM_ARCH=slc7_amd64_gcc700
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 
 
+# get CMSSW_10_2_13 for combine
+if [ -r CMSSW_10_2_13/src ] ; then
+ echo "Release CMSSW_10_2_13 already exists, using that."
+else
+  echo "Setting up CMSSW_10_2_13."
+  cmsrel CMSSW_10_2_13
+fi
+
 # get CMSSW_10_6_12
 if [ -r CMSSW_10_6_12/src ] ; then
  echo "Release CMSSW_10_6_12 already exists, using that."
@@ -20,6 +28,7 @@ cd ../../
 # download LEAF
 git clone https://github.com/reimersa/LEAF.git LEAF
 cd LEAF
+export LEAFPATH=$(readlink -f .)
 
 # clone JEC and JR databases into Analyzer
 cd Analyzer
@@ -33,6 +42,20 @@ cd HiggsAnalysis/CombinedLimit/
 git fetch origin
 git checkout v8.2.0
 cd ../../
+# cd $CMSSW_BASE/src
+# git clone https://github.com/cms-analysis/HiggsAnalysis-CombinedLimit.git HiggsAnalysis/CombinedLimit
+# cd HiggsAnalysis/CombinedLimit/
+# git fetch origin
+# git checkout v8.2.0
+# scramv1 b clean
+# scramv1 b
+# cd ../../
+# cmsrel CMSSW_10_2_13
+# cd CMSSW_10_2_13/src
+# cmsenv
+# git clone https://github.com/cms-analysis/HiggsAnalysis-CombinedLimit.git HiggsAnalysis/CombinedLimit
+# cd HiggsAnalysis/CombinedLimit
+# cd $LEAFPATH
 
 # all folders there, set up environment
 source setup.sh
@@ -42,11 +65,12 @@ cd $ANALYZERPATH
 make clean
 make -j
 
-cd $COMBINEPATH
+cd $PLOTTERPATH
 make clean
 make -j
 
-cd $PLOTTERPATH
+cd $COMBINEPATH
+source env_standalone.sh
 make clean
 make -j
 
--- setup.sh
+++ setup.sh
@@ -16,7 +16,18 @@ export PATH=${PATH}:${ANALYZERBINPATH}:${PLOTTERBINPATH}:${SUBMITTERPATH}
 export ROOT_INCLUDE_PATH=$ROOT_INCLUDE_PATH:$LEAFPATH
 export PYTHONPATH=$PYTHONPATH:$GENERATORPATH
 
-export COMBINEPATH=$(readlink -f HiggsAnalysis/CombinedLimit)
+# . /cvmfs/cms.cern.ch/${SCRAM_ARCH}/external/gcc/7.0.0-omkpbe2/etc/profile.d/init.sh
+# . /cvmfs/cms.cern.ch/${SCRAM_ARCH}/lcg/root/6.12.07-gnimlf5/bin/thisroot.sh
+# . /cvmfs/cms.cern.ch/${SCRAM_ARCH}/external/gsl/2.2.1-omkpbe2/etc/profile.d/init.sh
+# . /cvmfs/cms.cern.ch/${SCRAM_ARCH}/external/tbb/2018_U1-omkpbe2/etc/profile.d/init.sh
+# . /cvmfs/cms.cern.ch/${SCRAM_ARCH}/cms/vdt/0.4.0-gnimlf/etc/profile.d/init.sh
+# . /cvmfs/cms.cern.ch/${SCRAM_ARCH}/external/boost/1.63.0-gnimlf/etc/profile.d/init.sh
+# . /cvmfs/cms.cern.ch/${SCRAM_ARCH}/external/pcre/8.37-omkpbe2/etc/profile.d/init.sh
+# . /cvmfs/cms.cern.ch/${SCRAM_ARCH}/external/eigen/64060da8461a627eb25b5a7bc0616776068db58b/etc/profile.d/init.sh
+
+export CMSSW_COMBINE_BASE=$(readlink -f $LEAFPATH/../CMSSW_10_2_13)
+
+export COMBINEPATH=${CMSSW_BASE}/HiggsAnalysis/CombinedLimit
 export PATH=${PATH}:${COMBINEPATH}/exe:${COMBINEPATH}/scripts
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${COMBINEPATH}/lib
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${COMBINEPATH}/lib:
 export PYTHONPATH=${PYTHONPATH}:${COMBINEPATH}/lib/python:${COMBINEPATH}/lib
