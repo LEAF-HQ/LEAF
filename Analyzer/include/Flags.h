ad9b2d10955fef3bccfb9a0ff26290dd683319ab
Arne Christoph Reimers <arne.reimers@physik.uzh.ch>
2021-06-04 17:12:39 +0200

    Added wildcard option for flags, added flag histograms showing the trigger decisions

--- Analyzer/include/Flags.h
+++ Analyzer/include/Flags.h
@@ -15,8 +15,27 @@ public:
   ~Flags() = default;
 
   const bool has(const TString s) const {auto it = m_map.find(s); return (it != m_map.end());};
+  const bool has_prefix(const TString s) const {
+    if(m_map.lower_bound(s) != m_map.end()) return true;
+    return false;
+  };
   const bool get(const TString s) const {if(Flags::has(s)){return m_map.at(s);} else{throw runtime_error("Trying to get non-existent flag '" + s + "' from Flags class.");}};
+  const bool get_prefix(const TString s) const {
+    if(m_map.lower_bound(s) == m_map.end()) throw runtime_error("Trying to get non-existent flag with pre-fix '" + s + "' from Flags class.");
+    for(auto it=m_map.lower_bound(s); it != m_map.end(); ++it){
+      if(!it->first.Contains(s)) continue;
+      if(it->second) return true;
+    }
+    return false;
+  };
   void set(TString s, bool b) {m_map[s] = b;};
+
+
+  void list() {
+    std::cout << "Available flags:" << std::endl;
+    std::cout << "================" << std::endl << std::endl;
+    for (auto const & x : m_map) std::cout << x.first << std::endl;
+  };
 
 
 
--- Analyzer/src/FlagSelection.cc
+++ Analyzer/src/FlagSelection.cc
@@ -7,6 +7,24 @@ FlagSelection::FlagSelection(const Config & cfg, TString flagname_) : flagname(f
 
 bool FlagSelection::passes(RecoEvent & event){
 
-  return event.flags->get(flagname);
+  if(flagname.Contains("*")){
+    string s_flagname = flagname.Data();
+    TString flagname_nowildcard = flagname;
 
+    if(flagname_nowildcard(s_flagname.size() - 1) == TString("*")){
+      flagname_nowildcard = flagname_nowildcard(0,s_flagname.size() - 1);
+    }
+
+    if(flagname_nowildcard.Contains("*")) throw runtime_error("FlagSelection::passes(): Wildcard is only allowed in the very end of the flag name.");
+
+    if(event.flags->has_prefix(flagname_nowildcard)){
+      return event.flags->get_prefix(flagname_nowildcard);
+    }
+    return false;
+
+  }
+  else{
+    if(!event.flags->has(flagname)) return false;
+    return event.flags->get(flagname);
+  }
 }
