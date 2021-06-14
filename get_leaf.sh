0f59087eb321970e4e4623fa6987edb134b34ffc
Arne Christoph Reimers <arne.reimers@physik.uzh.ch>
2021-06-14 12:33:36 +0200

    Successfully implemented CI

--- get_leaf.sh
+++ get_leaf.sh
@@ -4,14 +4,6 @@
 export SCRAM_ARCH=slc7_amd64_gcc700
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 
-# get CMSSW_10_2_13 for combine
-if [ -r CMSSW_10_2_13/src ] ; then
- echo "Release CMSSW_10_2_13 already exists, using that."
-else
-  echo "Setting up CMSSW_10_2_13."
-  cmsrel CMSSW_10_2_13
-fi
-
 # get CMSSW_10_6_12 for UL
 if [ -r CMSSW_10_6_12/src ] ; then
  echo "Release CMSSW_10_6_12 already exists, using that."
@@ -42,21 +34,6 @@ cd HiggsAnalysis/CombinedLimit
 git fetch origin
 git checkout v8.2.0
 cd ../../
-
-# cd $CMSSW_BASE/src
-# git clone https://github.com/cms-analysis/HiggsAnalysis-CombinedLimit.git HiggsAnalysis/CombinedLimit
-# cd HiggsAnalysis/CombinedLimit/
-# git fetch origin
-# git checkout v8.2.0
-# scramv1 b clean
-# scramv1 b
-# cd ../../
-# cmsrel CMSSW_10_2_13
-# cd CMSSW_10_2_13/src
-# cmsenv
-# git clone https://github.com/cms-analysis/HiggsAnalysis-CombinedLimit.git HiggsAnalysis/CombinedLimit
-# cd HiggsAnalysis/CombinedLimit
-# cd $LEAFPATH
 
 # all folders there, set up environment
 cd $LEAFPATH
