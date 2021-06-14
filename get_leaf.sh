2d4654fc58920f0db7bb091db1f248d9c18f4da3
Arne Christoph Reimers <arne.reimers@physik.uzh.ch>
2021-06-14 12:07:59 +0200

    trying to find libGLU

--- get_leaf.sh
+++ get_leaf.sh
@@ -3,7 +3,6 @@
 # set up architecture
 export SCRAM_ARCH=slc7_amd64_gcc700
 source /cvmfs/cms.cern.ch/cmsset_default.sh
-echo $PWD
 
 # get CMSSW_10_2_13 for combine
 if [ -r CMSSW_10_2_13/src ] ; then
@@ -20,50 +19,30 @@ else
   echo "Setting up CMSSW_10_6_12."
   cmsrel CMSSW_10_6_12
 fi
-echo $PWD
 
 cd CMSSW_10_6_12/src
-echo $PWD
 eval `scramv1 runtime -sh`
 scram b
 cd ../../
-echo $PWD
 
 # download LEAF
 git clone https://github.com/reimersa/LEAF.git LEAF
 cd LEAF
-echo $PWD
 export LEAFPATH=$(readlink -f .)
-echo $LEAFPATH
-echo $PWD
 
 # clone JEC and JR databases into Analyzer
 cd $LEAFPATH/Analyzer
-echo $PWD
 git clone https://github.com/cms-jet/JECDatabase.git
 git clone https://github.com/cms-jet/JRDatabase.git
-echo $PWD
 
 # download Higgs Combine
 cd ../
-echo $PWD
 git clone https://github.com/cms-analysis/HiggsAnalysis-CombinedLimit.git HiggsAnalysis/CombinedLimit
-sleep 2
-ls -lrth
-echo $PWD
 cd HiggsAnalysis/CombinedLimit
-echo $PWD
-sleep 1
 git fetch origin
-echo $PWD
-sleep 1
 git checkout v8.2.0
-echo $PWD
-sleep 1
-ls -lrth
-sleep 1
 cd ../../
-echo $PWD
+
 # cd $CMSSW_BASE/src
 # git clone https://github.com/cms-analysis/HiggsAnalysis-CombinedLimit.git HiggsAnalysis/CombinedLimit
 # cd HiggsAnalysis/CombinedLimit/
@@ -80,56 +59,22 @@ echo $PWD
 # cd $LEAFPATH
 
 # all folders there, set up environment
-sleep 2
 cd $LEAFPATH
-sleep 2
-ls -lrth
-sleep 5
-echo $PWD
-sleep 5
 source $PWD/setup.sh
-
-sleep 2
-echo $PATH
-sleep 1
-echo $LD_LIBRARY_PATH
-sleep 1
-echo $CXXFLAGS
 
 # compile all!
 cd $ANALYZERPATH
-echo $PWD
-ls -lrth
-sleep 2
 make clean
-echo $PWD
-sleep 2
-make -j
-echo $PWD
+make
 
 cd $PLOTTERPATH
-echo $PWD
-sleep 2
 make clean
-echo $PWD
-sleep 2
-make -j
-echo $PWD
+make
 
 cd $COMBINEPATH
-echo $PWD
-sleep 2
-ls -lrth
-sleep 1
 source $PWD/env_standalone.sh
-echo $PWD
-sleep 2
 make clean
-echo $PWD
-sleep 2
-make -j
-echo $PWD
-sleep 2
+make
 
 cd $LEAFPATH
 echo $PWD
