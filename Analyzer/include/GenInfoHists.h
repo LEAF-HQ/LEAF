ac2b3608e8a7ca54a4426ed9290d18f2b69bb244
Arne Christoph Reimers <arne.reimers@cern.ch>
2021-07-14 11:50:09 +0200

    Added some more hists, reweighting to different PDF as well

--- Analyzer/include/GenInfoHists.h
+++ Analyzer/include/GenInfoHists.h
@@ -26,9 +26,9 @@ public:
 
 protected:
 
-  shared_ptr<TH1F> hptmet, hphimet, hgeninfoid1, hgeninfoid2, hgeninfox1, hgeninfox2, hgeninfoxpdf1, hgeninfoxpdf2, hgeninfoxpdf1_rebin1, hgeninfoxpdf2_rebin1, hgeninfoxpdf1_rebin2, hgeninfoxpdf2_rebin2, hgeninfoxpdfproduct, hgeninfoxpdfproduct_rebin1, hgeninfoxpdfproduct_rebin2, hgeninfoscalepdf, hsumweights;
+  shared_ptr<TH1F> hptmet, hphimet, hgeninfoid1, hgeninfoid2, hgeninfox1, hgeninfox2, hgeninfoxall, hgeninfoxb, hgeninfoxc, hgeninfoxpdf1, hgeninfoxpdf2, hgeninfoxpdf1_rebin1, hgeninfoxpdf2_rebin1, hgeninfoxpdf1_rebin2, hgeninfoxpdf2_rebin2, hgeninfoxpdf1_rebin3, hgeninfoxpdf2_rebin3, hgeninfoxpdfall, hgeninfoxpdfb, hgeninfoxpdfc, hgeninfoxpdfproduct, hgeninfoxpdfproduct_rebin1, hgeninfoxpdfproduct_rebin2, hgeninfoxpdfproduct_rebin3, hgeninfoxpdfproduct_rebin4, hgeninfoscalepdf, hsumweights,
 
-  LHAPDF::PDF* pdf;
-  double xmin, xmax, qmin, qmax;
+  hnewpdf_geninfoxpdf1, hnewpdf_geninfoxpdf2, hnewpdf_geninfoxpdf1_rebin1, hnewpdf_geninfoxpdf2_rebin1, hnewpdf_geninfoxpdf1_rebin2, hnewpdf_geninfoxpdf2_rebin2, hnewpdf_geninfoxpdf1_rebin3, hnewpdf_geninfoxpdf2_rebin3, hnewpdf_geninfoxpdfall, hnewpdf_geninfoxpdfb, hnewpdf_geninfoxpdfc, hnewpdf_geninfoxpdfproduct, hnewpdf_geninfoxpdfproduct_rebin1, hnewpdf_geninfoxpdfproduct_rebin2, hnewpdf_geninfoxpdfproduct_rebin3, hnewpdf_geninfoxpdfproduct_rebin4;
 
+  LHAPDF::PDF* pdf, *pdf_new;
 };
--- Analyzer/src/GenInfoHists.cc
+++ Analyzer/src/GenInfoHists.cc
@@ -28,15 +28,43 @@ GenInfoHists::GenInfoHists(TString dir_) : BaseHists(dir_){
   hgeninfoid2 = book<TH1F>("geninfoid2", ";ID of initial state 2; Events / bin", 11, -5.5, 5.5);
   hgeninfox1 = book<TH1F>("geninfox1", ";x of initial state 1; Events / bin", 50, 0, 1);
   hgeninfox2 = book<TH1F>("geninfox2", ";x of initial state 2; Events / bin", 50, 0, 1);
+  hgeninfoxall = book<TH1F>("geninfoxall", ";all x (both partons together); Events / bin", 50, 0, 1);
+  hgeninfoxb = book<TH1F>("geninfoxb", ";x of all b(bar) quarks; Events / bin", 50, 0, 1);
+  hgeninfoxc = book<TH1F>("geninfoxc", ";x of all c(bar) quarks; Events / bin", 50, 0, 1);
   hgeninfoxpdf1 = book<TH1F>("geninfoxpdf1", ";xpdf of initial state 1; Events / bin", 100, 0, 20);
   hgeninfoxpdf2 = book<TH1F>("geninfoxpdf2", ";xpdf of initial state 2; Events / bin", 100, 0, 20);
   hgeninfoxpdf1_rebin1 = book<TH1F>("geninfoxpdf1_rebin1", ";xpdf of initial state 1; Events / bin", 40, 0, 20);
   hgeninfoxpdf2_rebin1 = book<TH1F>("geninfoxpdf2_rebin1", ";xpdf of initial state 2; Events / bin", 40, 0, 20);
   hgeninfoxpdf1_rebin2 = book<TH1F>("geninfoxpdf1_rebin2", ";xpdf of initial state 1; Events / bin", 40, 0, 2);
   hgeninfoxpdf2_rebin2 = book<TH1F>("geninfoxpdf2_rebin2", ";xpdf of initial state 2; Events / bin", 40, 0, 2);
+  hgeninfoxpdf1_rebin3 = book<TH1F>("geninfoxpdf1_rebin3", ";xpdf of initial state 1; Events / bin", 40, 0, 0.2);
+  hgeninfoxpdf2_rebin3 = book<TH1F>("geninfoxpdf2_rebin3", ";xpdf of initial state 2; Events / bin", 40, 0, 0.2);
+  hgeninfoxpdfall = book<TH1F>("geninfoxpdfall", ";all xpdf (both partons together); Events / bin", 40, 0, 2);
+  hgeninfoxpdfb = book<TH1F>("geninfoxpdfb", ";xpdf of all b(bar) quarks; Events / bin", 40, 0, 2);
+  hgeninfoxpdfc = book<TH1F>("geninfoxpdfc", ";xpdf of all c(bar) quarks; Events / bin", 40, 0, 2);
   hgeninfoxpdfproduct = book<TH1F>("geninfoxpdfproduct", ";xpdf1 * xpdf2; Events / bin", 100, 0, 20);
   hgeninfoxpdfproduct_rebin1 = book<TH1F>("geninfoxpdfproduct_rebin1", ";xpdf1 * xpdf2; Events / bin", 40, 0, 20);
   hgeninfoxpdfproduct_rebin2 = book<TH1F>("geninfoxpdfproduct_rebin2", ";xpdf1 * xpdf2; Events / bin", 40, 0, 2);
+  hgeninfoxpdfproduct_rebin3 = book<TH1F>("geninfoxpdfproduct_rebin3", ";xpdf1 * xpdf2; Events / bin", 40, 0, 0.2);
+  hgeninfoxpdfproduct_rebin4 = book<TH1F>("geninfoxpdfproduct_rebin4", ";xpdf1 * xpdf2; Events / bin", 4000, 0, 20);
+
+  hnewpdf_geninfoxpdf1 = book<TH1F>("newpdf_geninfoxpdf1", ";new xpdf of initial state 1; Events / bin", 100, 0, 20);
+  hnewpdf_geninfoxpdf2 = book<TH1F>("newpdf_geninfoxpdf2", ";new xpdf of initial state 2; Events / bin", 100, 0, 20);
+  hnewpdf_geninfoxpdf1_rebin1 = book<TH1F>("newpdf_geninfoxpdf1_rebin1", ";new xpdf of initial state 1; Events / bin", 40, 0, 20);
+  hnewpdf_geninfoxpdf2_rebin1 = book<TH1F>("newpdf_geninfoxpdf2_rebin1", ";new xpdf of initial state 2; Events / bin", 40, 0, 20);
+  hnewpdf_geninfoxpdf1_rebin2 = book<TH1F>("newpdf_geninfoxpdf1_rebin2", ";new xpdf of initial state 1; Events / bin", 40, 0, 2);
+  hnewpdf_geninfoxpdf2_rebin2 = book<TH1F>("newpdf_geninfoxpdf2_rebin2", ";new xpdf of initial state 2; Events / bin", 40, 0, 2);
+  hnewpdf_geninfoxpdf1_rebin3 = book<TH1F>("newpdf_geninfoxpdf1_rebin3", ";new xpdf of initial state 1; Events / bin", 40, 0, 0.2);
+  hnewpdf_geninfoxpdf2_rebin3 = book<TH1F>("newpdf_geninfoxpdf2_rebin3", ";new xpdf of initial state 2; Events / bin", 40, 0, 0.2);
+  hnewpdf_geninfoxpdfall = book<TH1F>("newpdf_geninfoxpdfall", ";all new xpdf (both partons together); Events / bin", 40, 0, 2);
+  hnewpdf_geninfoxpdfb = book<TH1F>("newpdf_geninfoxpdfb", ";new xpdf of all b(bar) quarks; Events / bin", 40, 0, 2);
+  hnewpdf_geninfoxpdfc = book<TH1F>("newpdf_geninfoxpdfc", ";new xpdf of all c(bar) quarks; Events / bin", 40, 0, 2);
+  hnewpdf_geninfoxpdfproduct = book<TH1F>("newpdf_geninfoxpdfproduct", ";new xpdf1 * xpdf2; Events / bin", 100, 0, 20);
+  hnewpdf_geninfoxpdfproduct_rebin1 = book<TH1F>("newpdf_geninfoxpdfproduct_rebin1", ";new xpdf1 * new xpdf2; Events / bin", 40, 0, 20);
+  hnewpdf_geninfoxpdfproduct_rebin2 = book<TH1F>("newpdf_geninfoxpdfproduct_rebin2", ";new xpdf1 * new xpdf2; Events / bin", 40, 0, 2);
+  hnewpdf_geninfoxpdfproduct_rebin3 = book<TH1F>("newpdf_geninfoxpdfproduct_rebin3", ";new xpdf1 * new xpdf2; Events / bin", 40, 0, 0.2);
+  hnewpdf_geninfoxpdfproduct_rebin4 = book<TH1F>("newpdf_geninfoxpdfproduct_rebin4", ";new xpdf1 * new xpdf2; Events / bin", 4000, 0, 20);
+
   hgeninfoscalepdf = book<TH1F>("geninfoscalepdf", ";PDF scale; Events / bin", 200, 0, 3000);
 
   hsumweights = book<TH1F>("sumweights", ";bincontent = sum of event weights; Events / bin", 1, 0.5, 1.5);
@@ -45,6 +73,9 @@ GenInfoHists::GenInfoHists(TString dir_) : BaseHists(dir_){
   string pdfname = "NNPDF31_lo_as_0130";
   LHAPDF::initPDFSet(1, (std::string)(pdfname+".LHgrid"));
   pdf = LHAPDF::mkPDF( (std::string) pdfname, 0);
+  string pdfname_new = "NNPDF31_nnlo_hessian_pdfas";
+  LHAPDF::initPDFSet(1, (std::string)(pdfname_new+".LHgrid"));
+  pdf_new = LHAPDF::mkPDF( (std::string) pdfname_new, 0);
 
 }
 
@@ -66,10 +97,18 @@ void GenInfoHists::fill(const RecoEvent & event){
   hgeninfoid2->Fill(event.geninfo->id2(), weight);
   hgeninfox1->Fill(event.geninfo->x1(), weight);
   hgeninfox2->Fill(event.geninfo->x2(), weight);
+  hgeninfoxall->Fill(event.geninfo->x1(), weight);
+  hgeninfoxall->Fill(event.geninfo->x2(), weight);
+  if(abs(event.geninfo->id1()) == 5) hgeninfoxb->Fill(event.geninfo->x1(), weight);
+  if(abs(event.geninfo->id2()) == 5) hgeninfoxb->Fill(event.geninfo->x2(), weight);
+  if(abs(event.geninfo->id1()) == 4) hgeninfoxc->Fill(event.geninfo->x1(), weight);
+  if(abs(event.geninfo->id2()) == 4) hgeninfoxc->Fill(event.geninfo->x2(), weight);
   hgeninfoscalepdf->Fill(event.geninfo->scale_pdf(), weight);
 
   double xpdf1 = pdf->xfxQ(event.geninfo->id1(), event.geninfo->x1(), event.geninfo->scale_pdf());
   double xpdf2 = pdf->xfxQ(event.geninfo->id2(), event.geninfo->x2(), event.geninfo->scale_pdf());
+  double xpdf1_new = pdf_new->xfxQ(event.geninfo->id1(), event.geninfo->x1(), event.geninfo->scale_pdf());
+  double xpdf2_new = pdf_new->xfxQ(event.geninfo->id2(), event.geninfo->x2(), event.geninfo->scale_pdf());
 
   hgeninfoxpdf1->Fill(xpdf1, weight);
   hgeninfoxpdf2->Fill(xpdf2, weight);
@@ -77,23 +116,42 @@ void GenInfoHists::fill(const RecoEvent & event){
   hgeninfoxpdf2_rebin1->Fill(xpdf2, weight);
   hgeninfoxpdf1_rebin2->Fill(xpdf1, weight);
   hgeninfoxpdf2_rebin2->Fill(xpdf2, weight);
+  hgeninfoxpdf1_rebin3->Fill(xpdf1, weight);
+  hgeninfoxpdf2_rebin3->Fill(xpdf2, weight);
   hgeninfoxpdfproduct->Fill(xpdf1*xpdf2, weight);
   hgeninfoxpdfproduct_rebin1->Fill(xpdf1*xpdf2, weight);
   hgeninfoxpdfproduct_rebin2->Fill(xpdf1*xpdf2, weight);
+  hgeninfoxpdfproduct_rebin3->Fill(xpdf1*xpdf2, weight);
+  hgeninfoxpdfproduct_rebin4->Fill(xpdf1*xpdf2, weight);
 
-  // cout << "q2: " << event.geninfo->scale_pdf() << endl;
-  // cout << "id1: " << event.geninfo->id1() << ", x1: " << event.geninfo->x1() << ", xpdf1: " << xpdf1 << endl;
-  // cout << "id2: " << event.geninfo->id2() << ", x2: " << event.geninfo->x2() << ", xpdf2: " << xpdf2 << endl;
-  // cout << "xpdf1 * xpdf2: " << xpdf1 * xpdf2 << endl << endl;
+  hgeninfoxpdfall->Fill(xpdf1, weight);
+  hgeninfoxpdfall->Fill(xpdf2, weight);
+  if(abs(event.geninfo->id1()) == 5) hgeninfoxpdfb->Fill(xpdf1, weight);
+  if(abs(event.geninfo->id2()) == 5) hgeninfoxpdfb->Fill(xpdf2, weight);
+  if(abs(event.geninfo->id1()) == 4) hgeninfoxpdfc->Fill(xpdf1, weight);
+  if(abs(event.geninfo->id2()) == 4) hgeninfoxpdfc->Fill(xpdf2, weight);
 
 
-  // cout << " event.geninfo->id1(): " << event.geninfo->id1() << endl;
-  // cout << " event.geninfo->id2(): " << event.geninfo->id2() << endl;
-  // cout << " event.geninfo->x1(): " << event.geninfo->x1() << endl;
-  // cout << " event.geninfo->x2(): " << event.geninfo->x2() << endl;
-  // cout << " event.geninfo->xpdf1(): " << event.geninfo->xpdf1() << endl;
-  // cout << " event.geninfo->xpdf2(): " << event.geninfo->xpdf2() << endl;
-  // cout << " event.geninfo->scale_pdf(): " << event.geninfo->scale_pdf() << endl;
+  hnewpdf_geninfoxpdf1->Fill(xpdf1_new, weight);
+  hnewpdf_geninfoxpdf2->Fill(xpdf2_new, weight);
+  hnewpdf_geninfoxpdf1_rebin1->Fill(xpdf1_new, weight);
+  hnewpdf_geninfoxpdf2_rebin1->Fill(xpdf2_new, weight);
+  hnewpdf_geninfoxpdf1_rebin2->Fill(xpdf1_new, weight);
+  hnewpdf_geninfoxpdf2_rebin2->Fill(xpdf2_new, weight);
+  hnewpdf_geninfoxpdf1_rebin3->Fill(xpdf1_new, weight);
+  hnewpdf_geninfoxpdf2_rebin3->Fill(xpdf2_new, weight);
+  hnewpdf_geninfoxpdfproduct->Fill(xpdf1_new*xpdf2_new, weight);
+  hnewpdf_geninfoxpdfproduct_rebin1->Fill(xpdf1_new*xpdf2_new, weight);
+  hnewpdf_geninfoxpdfproduct_rebin2->Fill(xpdf1_new*xpdf2_new, weight);
+  hnewpdf_geninfoxpdfproduct_rebin3->Fill(xpdf1_new*xpdf2_new, weight);
+  hnewpdf_geninfoxpdfproduct_rebin4->Fill(xpdf1_new*xpdf2_new, weight);
+
+  hnewpdf_geninfoxpdfall->Fill(xpdf1_new, weight);
+  hnewpdf_geninfoxpdfall->Fill(xpdf2_new, weight);
+  if(abs(event.geninfo->id1()) == 5) hnewpdf_geninfoxpdfb->Fill(xpdf1_new, weight);
+  if(abs(event.geninfo->id2()) == 5) hnewpdf_geninfoxpdfb->Fill(xpdf2_new, weight);
+  if(abs(event.geninfo->id1()) == 4) hnewpdf_geninfoxpdfc->Fill(xpdf1_new, weight);
+  if(abs(event.geninfo->id2()) == 4) hnewpdf_geninfoxpdfc->Fill(xpdf2_new, weight);
 
 
   hsumweights->Fill(1, weight);
