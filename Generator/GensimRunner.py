116c0c3f312989174a411e7c5d8acc7b62f22cf6
Arne Christoph Reimers <arne.reimers@cern.ch>
2021-07-14 11:50:24 +0200

    Excluded some more SLURM nodes

--- Generator/GensimRunner.py
+++ Generator/GensimRunner.py
@@ -215,7 +215,7 @@ class GensimRunner:
                     if mode is 'new':        slurmjobname = '%s' % (self.folderstructure[generation_step]['jobnametag'])
                     elif mode is 'resubmit': slurmjobname = 'resubmit_%s' % (self.folderstructure[generation_step]['jobnametag'])
                     # jobs_per_sample_string = '%10' if mode == 'new' else ''
-                    command = 'sbatch -a 1-%s -J %s -p %s -t %s --exclude t3wn[49,50,54] --cpus-per-task %i submit_cmsRun_command.sh %s %s %s %s %s' % (str(njobs), slurmjobname+'_'+jobname, queue, runtime_str, ncores, self.generatorfolder, self.arch_tag, self.workarea+'/'+self.folderstructure[generation_step]['cmsswtag'], self.T2_director+self.T2_path+'/'+self.folderstructure[generation_step]['pathtag']+'/'+jobname, commandfilename)
+                    command = 'sbatch -a 1-%s -J %s -p %s -t %s --exclude t3wn[49,50,54,40,44,39] --cpus-per-task %i submit_cmsRun_command.sh %s %s %s %s %s' % (str(njobs), slurmjobname+'_'+jobname, queue, runtime_str, ncores, self.generatorfolder, self.arch_tag, self.workarea+'/'+self.folderstructure[generation_step]['cmsswtag'], self.T2_director+self.T2_path+'/'+self.folderstructure[generation_step]['pathtag']+'/'+jobname, commandfilename)
                     if njobs > 0:
                         if self.submit:
                             # print command
