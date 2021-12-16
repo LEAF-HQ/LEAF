bbaf5f0f3880f9296d70f287bb1195fbf4af3daa
Arne Christoph Reimers <arne.reimers@cern.ch>
2021-12-16 15:26:07 +0100

    Added class PlotterConfig to parse XML files

---
+++ Plotter/include/PlotterConfig.h
@@ -0,0 +1,67 @@
+#pragma once
+
+#include <cmath>
+#include <iostream>
+#include <TString.h>
+#include <TFile.h>
+
+#include "Analyzer/include/useful_functions.h"
+#include "Plotter/include/PlottingDataset.h"
+
+// struct plotting_dataset{
+//   TString name                = "";
+//   int     color               = -1;
+//   int     linestyle           = -1;
+//   TString legend              = "";
+//   TString type                = "";
+//   bool    stack               = false;
+//   TString infilename          = "";
+// };
+
+
+// Class to store the XML information and ultimately steer the looping through the different datasets.
+class PlotterConfig {
+
+public:
+  // Constructors, destructor
+  PlotterConfig(TString configfilename);
+  PlotterConfig(const PlotterConfig &) = default;
+  PlotterConfig & operator = (const PlotterConfig &) = default;
+  ~PlotterConfig() = default;
+
+  void increment_idx(){m_idx += 1;};
+  void set_idx(size_t i){m_idx = i;};
+  void add_dataset(PlottingDataset d){m_datasets.emplace_back(d);};
+  const size_t  n_datasets()   const {return m_datasets.size();};
+  const size_t  idx()          const {return m_idx;};
+
+  const TString output_directory() const {return m_output_directory;};
+  const TString input_directory() const {return m_input_directory;};
+  const TString se_director() const {return m_se_director;};
+  const TString prefix() const {return m_prefix;};
+  const TString lumitext() const {return m_lumitext;};
+  const bool blind() const {return m_blind;};
+  const bool logY() const {return m_logY;};
+  const bool normalize() const {return m_normalize;};
+  const bool singlePDF() const {return m_singlePDF;};
+  const TString numerator() const {return m_numerator;};
+  const std::vector<PlottingDataset> datasets() const {return m_datasets;};
+
+  const TString dataset_name() const {return m_datasets[m_idx].name;};
+  const int dataset_color() const {return m_datasets[m_idx].color;};
+  const int dataset_linestyle() const {return m_datasets[m_idx].linestyle;};
+  const TString dataset_legend() const {return m_datasets[m_idx].legend;};
+  const TString dataset_type() const {return m_datasets[m_idx].type;};
+  const bool dataset_stack() const {return m_datasets[m_idx].stack;};
+
+
+
+
+private:
+  TString m_input_directory, m_output_directory, m_se_director, m_prefix, m_lumitext, m_numerator;
+  bool m_blind, m_logY, m_normalize, m_singlePDF;
+  std::vector<PlottingDataset> m_datasets = {};
+  int m_idx = 0;
+  bool m_is_init = false;
+
+};
---
+++ Plotter/src/PlotterConfig.cc
@@ -0,0 +1,69 @@
+#include <TString.h>
+#include <TFile.h>
+#include <iostream>
+#include <sys/stat.h>
+
+#include "Plotter/include/PlotterConfig.h"
+#include "Analyzer/include/useful_functions.h"
+
+#include <libxml/parser.h>
+#include <libxml/tree.h>
+#include <libxml/xmlreader.h>
+
+
+using namespace std;
+
+PlotterConfig::PlotterConfig(TString configfilename){
+  if(m_is_init) throw runtime_error("PlotterConfig object already initialized. Abort.");
+
+  validateConfigFile(configfilename);
+  xmlDoc *doc = xmlReadFile(configfilename, NULL, XML_PARSE_DTDATTR | XML_PARSE_NOENT | XML_PARSE_DTDVALID);
+  xmlNode *root_element = xmlDocGetRootElement(doc);
+
+  // Read general job information
+  // ============================
+
+  // get file configs
+  xmlNode *node_file_config = findNodeByName(root_element, "FileConfiguration");
+  m_output_directory = getNodeProperty(node_file_config, "OutputDirectory");
+  m_input_directory  = getNodeProperty(node_file_config, "InputDirectory");
+  m_se_director      = getNodeProperty(node_file_config, "SEDirector");
+  m_prefix           = getNodeProperty(node_file_config, "PreFix");
+
+  // get plotting configs
+  xmlNode *node_plotting_config = findNodeByName(root_element, "PlottingConfiguration");
+  m_lumitext  = getNodeProperty(node_plotting_config, "lumitext");
+  m_blind     = ("true" == getNodeProperty(node_plotting_config, "blind"));
+  m_logY      = ("true" == getNodeProperty(node_plotting_config, "logY"));
+  m_normalize = ("true" == getNodeProperty(node_plotting_config, "normalize"));
+  m_singlePDF = ("true" == getNodeProperty(node_plotting_config, "singlePDF"));
+  m_numerator = getNodeProperty(node_plotting_config, "numerator");
+
+  // Loop through InputDatasets and extract their information
+  // ========================================================
+
+  xmlNode *node_inputdatasets = findNodeByName(root_element, "InputDatasets");
+
+  for (xmlNode* current_node = node_inputdatasets->children; current_node; current_node = current_node->next){
+    if(current_node->type == XML_ELEMENT_NODE){
+
+      PlottingDataset ds(
+        getNodeProperty(current_node, "Name"),
+        stoi(getNodeProperty(current_node, "Color")),
+        stoi(getNodeProperty(current_node, "Linestyle")),
+        getNodeProperty(current_node, "Legend"),
+        getNodeProperty(current_node, "Type"),
+        ("true" == getNodeProperty(current_node, "Stack")),
+        m_input_directory + "/" + (TString)getNodeProperty(current_node, "Type") + "__" + (TString)getNodeProperty(current_node, "Name") + ".root"
+      );
+
+      m_datasets.emplace_back(ds);
+    }
+  }
+
+
+
+  xmlFreeDoc(doc);
+  xmlCleanupParser();
+  m_is_init = true;
+}
