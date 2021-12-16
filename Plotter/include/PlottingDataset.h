f1e9d3c13ddf3ec4b423a6ad8ec51ec39c4f21b0
Arne Christoph Reimers <arne.reimers@cern.ch>
2021-12-16 15:26:59 +0100

    Added class PlottingDataset to store info on dataset

---
+++ Plotter/include/PlottingDataset.h
@@ -0,0 +1,78 @@
+#pragma once
+
+#include <cmath>
+#include <iostream>
+#include <TString.h>
+#include <TFile.h>
+
+#include "Analyzer/include/useful_functions.h"
+
+
+
+// Simple container class + functionality to load and delete infiles
+
+class PlottingDataset {
+
+public:
+  // Constructors, destructor
+  PlottingDataset(TString name_="", int color_=-1, int linestyle_=-1, TString legend_="", TString type_="", bool stack_=false, TString infilename_="") : name(name_), color(color_), linestyle(linestyle_), legend(legend_), type(type_), stack(stack_), infilename(infilename_){
+    is_infile_populated = false;
+    infile = nullptr;
+  };
+
+  PlottingDataset(const PlottingDataset &) = default;
+  PlottingDataset & operator = (const PlottingDataset &) = default;
+  ~PlottingDataset(){
+    ClearInfile();
+    name                = "";
+    color               = -1;
+    linestyle           = -1;
+    legend              = "";
+    type                = "";
+    stack               = false;
+    infilename          = "";
+  };
+
+  void PopulateInfile(){
+    if(!is_infile_populated){
+      infile = new TFile(infilename, "READ");
+      is_infile_populated = true;
+    }
+  };
+
+  void ClearInfile(){
+
+    if(is_infile_populated){
+      infile->cd();
+
+      if(infile->IsOpen()){
+        infile->Close();
+      }
+
+      delete infile;
+      infile = nullptr;
+      is_infile_populated = false;
+    }
+  };
+
+
+
+  TString name;
+  int     color;
+  int     linestyle;
+  TString legend;
+  TString type;
+  bool    stack;
+  TString infilename;
+  TFile*  infile;
+  bool    is_infile_populated;
+
+
+};
+
+
+
+
+
+
+//
