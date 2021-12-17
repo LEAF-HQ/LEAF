094258088626a252dfa43dea7ba500049764019c
Arne Christoph Reimers <arne.reimers@cern.ch>
2021-12-17 12:55:03 +0100

    Made setup script standalone

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 # This script must be sourced from this exact folder before working in this folder.
 
-
-# Append path to our own libraries and paths to LD_LIBRARY_PATH and PATH and PYTHONPATH, such that our programs can use them!
+# Set up LEAF folder structure
 export LEAFPATH=$(readlink -f .)
 export ANALYZERPATH=$(readlink -f Analyzer)
 export ANALYZERLIBPATH=${ANALYZERPATH}/lib
@@ -12,6 +11,15 @@ export PLOTTERBINPATH=${PLOTTERPATH}/bin
 export GENERATORPATH=$(readlink -f Generator)
 export SAMPLESPATH=$(readlink -f Samples)
 
+# Set up correct CMSSW version and scram architecure
+export SCRAM_ARCH=slc7_amd64_gcc700
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+cd $LEAFPATH/../CMSSW_10_6_12/src
+eval `scramv1 runtime -sh`
+cd $LEAFPATH
+
+
+# Append path to our own libraries and paths to LD_LIBRARY_PATH and PATH and PYTHONPATH, such that our programs can use them!
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ANALYZERLIBPATH
 export PATH=${PATH}:${ANALYZERBINPATH}:${PLOTTERBINPATH}:${SUBMITTERPATH}
 export ROOT_INCLUDE_PATH=$ROOT_INCLUDE_PATH:$LEAFPATH
