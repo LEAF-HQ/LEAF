8140ca41efef3163a3eb40a82ebd04cd258606af
Andrea Malara <andrea.malara@cern.ch>
2022-01-11 16:14:30 +0100

    Simplify class

--- Samples/Storage.py
+++ Samples/Storage.py
@@ -3,64 +3,58 @@ import subprocess
 from utils import *
 
 class Storage():
-    def __init__(self, path):
-        self.director = None
+    def __init__(self, path = None, director = None):
         self.path = path
+        self.director = director
 
-
-class Storage_DAS(Storage):
-    def __init__(self, path):
-        self.director = 'root://cms-xrd-global.cern.ch/'
-        self.path = path
-        # self.filelist = self.get_file_list()
+    def create_file_list(self):
+        raise NotImplementedError()
 
     def get_file_list(self):
-        #call 'dasgoclient' to get a list of rootfiles from the DAS string
-        dasgocommand = 'dasgoclient -query="file dataset=%s"' % (self.path)
-        filelist = subprocess.check_output(dasgocommand, shell=True).split('\n')
-        newfiles = []
-        for f in filelist:
-            if f is '': continue
-            newfiles.append(self.director + f)
-        return newfiles
+        return self.create_file_list()
 
-#gsiftp://storage01.lcg.cscs.ch/
-class Storage_T2PSI(Storage):
-    def __init__(self, path):
-        self.director = 'root://storage01.lcg.cscs.ch/'
-        self.path = '/pnfs/lcg.cscs.ch/cms/trivcat' + path
-        # self.filelist = self.get_file_list()
-
-    def get_file_list(self):
-        command = 'LD_LIBRARY_PATH='' PYTHONPATH='' gfal-ls %s' % (self.director.replace('root://', 'gsiftp://')+self.path)
+    def make_list(self, command, outpath=""):
         filelist = subprocess.check_output(command, shell=True).split('\n')
         newfiles = []
         for f in filelist:
             if f is '': continue
-            newfiles.append(os.path.join(self.director+self.path, f))
+            newfiles.append(os.path.join(outpath, f))
         return newfiles
+
+
+class Storage_DAS(Storage):
+    def __init__(self, path):
+        Storage.__init__(self, path = path, director = 'root://cms-xrd-global.cern.ch/')
+
+    def create_file_list(self):
+        #call 'dasgoclient' to get a list of rootfiles from the DAS string
+        dasgocommand = 'dasgoclient -query="file dataset=%s"' % (self.path)
+        return self.make_list(dasgocommand, outpath=self.director)
+
+#gsiftp://storage01.lcg.cscs.ch/
+class Storage_T2PSI(Storage):
+    def __init__(self, path):
+        Storage.__init__(self, path = '/pnfs/lcg.cscs.ch/cms/trivcat' + path, director = 'root://storage01.lcg.cscs.ch/')
+
+    def create_file_list(self):
+        command = 'LD_LIBRARY_PATH='' PYTHONPATH='' gfal-ls %s' % (self.director.replace('root://', 'gsiftp://')+self.path)
+        return self.make_list(command, outpath=self.director+self.path)
 
 
 class Storage_T3PSI(Storage):
     def __init__(self, path):
-        self.director = 'root://t3dcachedb03.psi.ch/'
-        self.path = '/pnfs/psi.ch/cms/trivcat' + path
-        # self.filelist = self.get_file_list()
+        Storage.__init__(self, path = '/pnfs/psi.ch/cms/trivcat' + path, director = 'root://t3dcachedb03.psi.ch/')
 
     # if the path does not yet exist, create it
     def make_dirs(self):
         ensureDirectory(self.path)
 
-    def get_file_list(self):
+    def create_file_list(self):
         if not os.path.exists(self.path):
             return []
         command = 'ls %s' % (self.path)
-        filelist = subprocess.check_output(command, shell=True).split('\n')
-        newfiles = []
-        for f in filelist:
-            if f is '': continue
-            newfiles.append(os.path.join(self.director+self.path, f))
-        return newfiles
+        return self.make_list(command, outpath=self.director+self.path)
+
 
 
 
