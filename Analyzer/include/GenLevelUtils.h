fa10b4835fc47c95062187dedaef61af48ba7371
anmalara <andrea.malara@cern.ch>
2022-03-09 10:46:07 +0100

    Add enum for PF types. Moved to enum class

--- Analyzer/include/GenLevelUtils.h
+++ Analyzer/include/GenLevelUtils.h
@@ -8,8 +8,15 @@
 #include "LEAF/Analyzer/include/GenLevelConstants.h"
 
 bool isLeptonic(int pdgId);
+bool isLeptonic(ParticleID pdgId);
 bool isHadronic(int pdgId);
-bool isDecayMode(int pdgId1, int pdgId2, Decay decay);
+bool isHadronic(ParticleID pdgId);
+bool isDecayMode(int pdgId1, ParticleID pdgId2, Decay decay);
+bool isDecayMode(ParticleID pdgId1, ParticleID pdgId2, Decay decay);
 
 std::string pdgId2str(int);
+std::string pdgId2str(ParticleID);
+std::string type2str(int);
+std::string type2str(ParticleType);
 std::string decay2str(int);
+std::string decay2str(Decay);
