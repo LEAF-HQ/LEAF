310a7f852a9a9683090faa11ab2c92407591b582
Arne Christoph Reimers <arne.reimers@cern.ch>
2022-06-13 14:49:25 +0200

    Generalizing plotting function, add support for predictions

--- DNNTools/PlotterBase.py
+++ DNNTools/PlotterBase.py
@@ -5,8 +5,6 @@ import pandas as pd
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
-# matplotlib.style.use('default')
-# matplotlib.style.use('seaborn')
 
 from printing_utils import green, blue
 from DNNutils import SaveMPL, LoadPandas
@@ -14,9 +12,10 @@ from functions_dnn import classes_to_str, float_to_str
 
 
 class PlotterBase():
-    def __init__(self, inputdir='', outdir='', runonfraction = 1.0):
+    def __init__(self, inputdir='', outdir='', predictiondir='', runonfraction=1.0):
         self.inputdir = inputdir
         self.outdir = outdir
+        self.predictiondir = predictiondir
         self.frac = float_to_str(runonfraction)
 
     def DefineCommonStyle(self):
@@ -37,31 +36,61 @@ class PlotterBase():
         raise NotImplementedError('DefineClasses method is not initialized. Fix this.')
 
     def LoadInputsBase(self, format='csv'):
-        print(blue('--> Loading'))
+        print(blue('--> Loading inputs (base)'))
         inputdir = os.path.join(self.inputdir, classes_to_str(self.classes))
         inputs = []
         for label in ['train', 'val', 'test']:
+            print(blue('  --> loading for subset \'%s\'' % (label)))
             input   = LoadPandas(os.path.join(inputdir, 'input_%s_%s.%s' %(label,self.frac,format) ))
             input['label'] = np.load(os.path.join(inputdir, 'label_%s_%s.npy' %(label,self.frac) )).tolist()
             input['label'] = np.argmax(np.array(input['label'].to_list()), axis = 1)
             input['weights'] = LoadPandas(os.path.join(inputdir, 'weights_%s_%s.%s' %(label,self.frac,format) ))
             inputs.append(input)
-        self.df = pd.concat(inputs)
+            print(blue('  --> loaded for %s' % (label)))
+        self.inputs = pd.concat(inputs)
+        print(green('--> Loaded inputs (base)'))
 
     def LoadInputs(self):
         self.LoadInputsBase()
 
-    def PlotSingleVariable(self, style, variable_name, ylabel='Number of events / bin', yscale='log'):
+    def EnsureInputsLoaded(self):
+        if not hasattr(self, 'inputs'):
+            self.LoadInputs()
+
+
+    def LoadPredictionsBase(self):
+        print(blue('--> Loading predictions (base)'))
+        inputdir = os.path.join(self.inputdir, classes_to_str(self.classes))
+        preds = []
+        for label in ['train', 'val', 'test']:
+            print(blue('  --> Loading for subset \'%s\'' % (label)))
+            pred   = LoadPandas(os.path.join(os.path.join(self.predictiondir), 'prediction_%s_%s.csv' %(label,self.frac) ))
+            pred['label'] = np.load(os.path.join(inputdir, 'label_%s_%s.npy' %(label,self.frac) )).tolist()
+            pred['label'] = np.argmax(np.array(pred['label'].to_list()), axis = 1)
+            pred['weights'] = LoadPandas(os.path.join(inputdir, 'weights_%s_%s.csv' %(label,self.frac) ))
+            preds.append(pred)
+            print(blue('  --> Loaded for subset \'%s\'' % (label)))
+        self.predictions = pd.concat(preds)
+        print(green('--> Loaded predictions (base)'))
+
+    def LoadPredictions(self):
+        self.LoadPredictionsBase()
+
+    def EnsurePredictionsLoaded(self):
+        if not hasattr(self, 'predictions'):
+            self.LoadPredictions()
+
+    def PlotSingleVariable(self, df, style, variable_name, ylabel='Number of events / bin', yscale='log'):
         plt.clf()
         fig = plt.figure()
         classes = list(set(self.classes.values()))
 
-        hist_min = self.df[variable_name].min()
-        hist_max = self.df[variable_name].max()
+        hist_min = df[variable_name].min()
+        hist_max = df[variable_name].max()
         for cl in classes:
-            mask = self.df['label']==cl
-            weights_thisclass = self.df[mask]['weights']
-            df_thisclass = self.df[mask][variable_name]
+            mask = df['label']==cl
+            weights_thisclass = df[mask]['weights']
+            df_thisclass = df[mask][variable_name]
 
             style_thisclass = style[cl]
             style_thisclass.update(self.common_style)
@@ -69,7 +98,7 @@ class PlotterBase():
                 style_thisclass.update(self.stylePerVariable[variable_name])
 
             plt.hist(df_thisclass, weights=weights_thisclass, range=(hist_min, hist_max), **style_thisclass)
-            
+
         plt.legend(loc='best')
         plt.yscale(yscale)
         plt.xlabel(variable_name)
@@ -79,13 +108,13 @@ class PlotterBase():
         plt.close()
 
 
-    def PlotBase(self, style):
-        for variable_name in tqdm.tqdm(self.df.columns, desc="Plots done"):
+    def PlotBase(self, df, style):
+        for variable_name in tqdm.tqdm(df.columns, desc="Plots done"):
             if 'label' in variable_name: continue
-            self.PlotSingleVariable(style, variable_name)
+            self.PlotSingleVariable(df=df, style=style, variable_name=variable_name)
 
     def Plot(self):
         self.DefineCommonStyle()
         self.DefineStylePerVariable()
-        self.LoadInputs()
-        self.PlotBase(self.DefineStyle())
+        self.EnsureInputsLoaded()
+        self.PlotBase(df=self.inputs, style=self.DefineStyle())
